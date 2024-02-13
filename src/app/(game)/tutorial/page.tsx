4cd05092a51d132a8e67c38b5a7a3ce401b76139
gabriel <gabrieldasnevespinheiro@gmail.com>
2024-02-13 18:15:28 -0300

    responsive layout

--- src/app/(game)/tutorial/page.tsx
+++ src/app/(game)/tutorial/page.tsx
@@ -20,37 +20,37 @@ export default function Tutorial() {
 
     useEffect(() => {
         setHints([
-            <h1 className="text-4xl" key={"presentation"}>Color Rect é um jogo de <strong className="text-green-500">resposta rápida</strong>.</h1>,
+            <h1 className="md:text-4xl text-2xl" key={"presentation"}>Color Rect é um jogo de <strong className="text-green-500">resposta rápida</strong>.</h1>,
 
-            <h1 className="text-4xl" key={"game rule"}>Você deve clicar no botão com a <strong className="text-blue-400">cor</strong> que preenche o <strong className="text-blue-400">texto</strong>.</h1>,
+            <h1 className="md:text-4xl text-2xl" key={"game rule"}>Você deve clicar no botão com a <strong className="text-blue-400">cor</strong> que preenche o <strong className="text-blue-400">texto</strong>.</h1>,
 
             <div className="flex flex-col items-center space-y-4" key={"game tutorial"}>
-                <h1 className="text-4xl">Caso você se depare com o texto</h1><ColorHeader color={"Laranja"}>Amarelo</ColorHeader>
-                <span className="text-3xl">Você deve selecionar</span>
+                <h1 className="md:text-4xl text-xl">Caso você se depare com o texto</h1><ColorHeader color={"Laranja"} className="text-3xl md:text-7xl">Amarelo</ColorHeader>
+                <span className="md:text-3xl text-xl">Você deve selecionar</span>
 
                 <div className="flex flex-col items-center">
                     <ArrowDown className="animate-bounce" />
                     <div className="flex rounded-md overflow-hidden">
-                        <Colorpad color={"Amarelo"} />
+                        <Colorpad color={"Amarelo"} className="hidden md:block" />
                         <Colorpad color={"Laranja"} />
-                        <Colorpad color={"Verde"} />
+                        <Colorpad color={"Verde"} className="hidden md:block" />
                     </div>
                 </div>
             </div>,
 
-            <div className="flex flex-col space-y-4 text-center" key={"timer tutorial"}>
-                <h1 className="text-4xl">Fique de olho no <strong className="text-purple-500">cronômetro</strong>!</h1>
-                <h2 className="text-4xl">Se ele zerar você <strong className="text-red-500">perde</strong>,</h2>
-                <h2 className="text-4xl">Ele <strong className="text-green-500">reseta</strong> sempre que você acerta.</h2>
+            <div className="flex flex-col md:space-y-4 space-y-2 text-center" key={"timer tutorial"}>
+                <h1 className="md:text-4xl text-2xl">Fique de olho no <strong className="text-purple-500">cronômetro</strong>!</h1>
+                <h2 className="md:text-4xl text-2xl">Se ele zerar você <strong className="text-red-500">perde</strong>,</h2>
+                <h2 className="md:text-4xl text-2xl">Ele <strong className="text-green-500">reseta</strong> sempre que você acerta.</h2>
                 <div className="flex items-center space-x-2 justify-center animate-pulse">
-                    <TimerIcon size={90} className="text-purple-500" />
-                    <p className="text-7xl text-purple-500">{Math.abs(2.00).toFixed(1)}</p>
+                    <TimerIcon className="text-purple-500 md:w-32 md:h-32 w-12 h-12" />
+                    <p className="md:text-7xl text-3xl text-purple-500">{Math.abs(2.00).toFixed(1)}</p>
                 </div>
 
             </div>,
 
             <div className="flex flex-coll" key={"gamemodes tutorial"}>
-                <h1 className="text-4xl">Existem <strong className="text-orange-500">dificuldades</strong>, elas modificam o <strong className="text-purple-500">cronômetro</strong> e a <strong className="text-yellow-500">pontuação</strong>.</h1>
+                <h1 className="md:text-4xl text-2xl">Existem <strong className="text-orange-500">dificuldades</strong>, elas modificam o <strong className="text-purple-500">cronômetro</strong> e a <strong className="text-yellow-500">pontuação</strong>.</h1>
             </div>,
 
             <div className="flex flex-col space-y-4" key={"ending"}>
@@ -61,14 +61,14 @@ export default function Tutorial() {
     }, []);
 
     return (
-        <div className="flex flex-col h-screen w-full items-center p-10">
+        <div className="flex flex-col h-screen w-full items-center md:p-10 text-center md:text-start">
             <h1 className="text-3xl">{page + 1} / {hints.length}</h1>
-            <div className="flex h-full w-full  justify-between items-center p-4">
-                <ArrowLeft size={50} onClick={() => setPage(page < 1 ? 0 : page - 1)} className="hover:cursor-pointer" />
+            <div className="flex h-full w-full  justify-between items-center md:p-4">
+                <ArrowLeft onClick={() => setPage(page < 1 ? 0 : page - 1)} className="hover:cursor-pointer min-h-12 min-w-12" />
                 <div className="flex flex-col p-5 space-y-3 items-center">
                     {hints[page]}
                 </div>
-                <ArrowRight size={50} onClick={() => setPage(page >= hints.length - 1 ? page : page + 1)} className="hover:cursor-pointer" />
+                <ArrowRight onClick={() => setPage(page >= hints.length - 1 ? page : page + 1)} className="hover:cursor-pointer min-h-12 min-w-12" />
             </div>
         </div>
     )
